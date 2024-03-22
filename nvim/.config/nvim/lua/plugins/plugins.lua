0288556e70b316d5d37396506774758ab58eb6e2
Alex Castle <acastle@plex.com>
2024-03-22 10:17:37 -0700

    Add nvim-dap config

--- nvim/.config/nvim/lua/plugins/plugins.lua
+++ nvim/.config/nvim/lua/plugins/plugins.lua
@@ -30,6 +30,32 @@ return {
     end,
   },
   {
+    "mfussenegger/nvim-dap",
+    keys = {
+      {
+        "<F8>",
+        function()
+          require("dap").step_out()
+        end,
+        desc = "Step Out",
+      },
+      {
+        "<F10>",
+        function()
+          require("dap").step_over()
+        end,
+        desc = "Step Over",
+      },
+      {
+        "<F5>",
+        function()
+          require("dap").continue()
+        end,
+        desc = "Continue",
+      },
+    },
+  },
+  {
     "hrsh7th/nvim-cmp",
     dependencies = {
       "hrsh7th/cmp-emoji",
