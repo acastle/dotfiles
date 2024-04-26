413e401c3e572bbff92d9ddabcaedc7d8208f55c
Alex Castle <alex.castle@rockwellautomation.com>
2024-04-26 11:41:46 -0700

    Add support for DAP and update nvim plugins

--- nvim/.config/nvim/lua/plugins/c_sharp.lua
+++ nvim/.config/nvim/lua/plugins/c_sharp.lua
@@ -1,19 +1,38 @@
+vim.g.dotnet_build_project = function()
+  local default_path = vim.fn.getcwd() .. "/"
+  if vim.g["dotnet_last_proj_path"] ~= nil then
+    default_path = vim.g["dotnet_last_proj_path"]
+  end
+  local path = vim.fn.input("Path to your *proj file", default_path, "file")
+  vim.g["dotnet_last_proj_path"] = path
+  local cmd = "dotnet build -c Debug " .. path .. " > /dev/null"
+  print("")
+  print("Cmd to execute: " .. cmd)
+  local f = os.execute(cmd)
+  if f == 0 then
+    print("\nBuild: ✔️ ")
+  else
+    print("\nBuild: ❌ (code: " .. f .. ")")
+  end
+end
+
+vim.g.dotnet_get_dll_path = function()
+  local request = function()
+    return vim.fn.input("Path to dll", vim.fn.getcwd() .. "/bin/Debug/", "file")
+  end
+
+  if vim.g["dotnet_last_dll_path"] == nil then
+    vim.g["dotnet_last_dll_path"] = request()
+  else
+    if vim.fn.confirm("Do you want to change the path to dll?\n" .. vim.g["dotnet_last_dll_path"], "&yes\n&no", 2) == 1 then
+      vim.g["dotnet_last_dll_path"] = request()
+    end
+  end
+
+  return vim.g["dotnet_last_dll_path"]
+end
+
 local includes = {
-  -- {
-  --   "jmederosalvarado/roslyn.nvim",
-  --   dependencies = {
-  --     "neovim/nvim-lspconfig",
-  --   },
-  --   config = function()
-  --     local capabilities = require("cmp_nvim_lsp").default_capabilities()
-  --     require("roslyn").setup({
-  --       dotnet_cmd = "dotnet",
-  --       roslyn_version = "4.8.0-3.23475.7",
-  --       capabilities = capabilities,
-  --       on_attach = function() end,
-  --     })
-  --   end,
-  -- },
   { "Hoffs/omnisharp-extended-lsp.nvim", lazy = true },
   {
     "nvim-treesitter/nvim-treesitter",
@@ -83,33 +102,43 @@ local includes = {
   },
   {
     "mfussenegger/nvim-dap",
-    optional = true,
-    opts = function()
+    opts = function(_, opts)
       local dap = require("dap")
-      if not dap.adapters["netcoredbg"] then
-        require("dap").adapters["netcoredbg"] = {
-          type = "executable",
-          command = vim.fn.exepath("netcoredbg"),
-          args = { "--interpreter=vscode" },
+      dap.adapters["netcoredbg"] = {
+        type = "executable",
+        command = vim.fn.exepath("netcoredbg"),
+        args = { "--interpreter=vscode" },
+      }
+      for _, lang in ipairs({ "cs", "fsharp", "vb" }) do
+        dap.configurations[lang] = {
+          {
+            type = "netcoredbg",
+            name = "Launch file",
+            request = "launch",
+            ---@diagnostic disable-next-line: redundant-parameter
+            program = function()
+              if vim.fn.confirm("Should I recompile first?", "&yes\n&no", 2) == 1 then
+                vim.g.dotnet_build_project()
+              end
+              return vim.g.dotnet_get_dll_path()
+            end,
+            cwd = "${workspaceFolder}",
+          },
         }
       end
-      for _, lang in ipairs({ "cs", "fsharp", "vb" }) do
-        if not dap.configurations[lang] then
-          dap.configurations[lang] = {
-            {
-              type = "netcoredbg",
-              name = "Launch file",
-              request = "launch",
-              ---@diagnostic disable-next-line: redundant-parameter
-              program = function()
-                return vim.fn.input("Path to dll: ", vim.fn.getcwd() .. "/", "file")
-              end,
-              cwd = "${workspaceFolder}",
-            },
-          }
-        end
-      end
     end,
+  },
+  {
+    "nvim-neotest/neotest",
+    optional = true,
+    dependencies = {
+      "issafalcon/neotest-dotnet",
+    },
+    opts = {
+      adapters = {
+        ["neotest-dotnet"] = {},
+      },
+    },
   },
 }
 
